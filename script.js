9b1e165b60bbe690f866f3a50215219831fbf31d
Abu-alwafa <abu.alwafa892@gmail.com>
2022-04-16 21:23:06 +0300

    add project files

---
+++ script.js
@@ -0,0 +1,102 @@
+const X_CLASS = "x";
+const CIRCLE_CLASS = "circle";
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+const cellElements = document.querySelectorAll("[data-cell]");
+const board = document.getElementById("board");
+const winnerMessageElement = document.getElementById("winningMassage");
+const winnerMessageText = document.querySelector("[data-winning-massage-text]");
+const restartButton = document.getElementById("restartButton");
+const xScoreText = document.getElementById("xScore");
+const oScoreText = document.getElementById("oScore");
+
+let circleTurn;
+let xScore = 0,
+    oScore = 0;
+startGame();
+
+restartButton.addEventListener("click", startGame);
+
+function startGame() {
+    circleTurn = false;
+    setBorderHoverClass();
+    cellElements.forEach((cell) => {
+        cell.classList.remove(X_CLASS);
+        cell.classList.remove(CIRCLE_CLASS);
+        cell.removeEventListener("click", handleClick);
+        cell.addEventListener("click", handleClick, { once: true });
+    });
+    winnerMessageElement.classList.remove("show");
+    xScoreText.textContent = xScore;
+    oScoreText.textContent = oScore;
+}
+
+function handleClick(e) {
+    const cell = e.target;
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+
+    placeMark(cell, currentClass);
+    if (checkWin(currentClass)) {
+        endGame(false);
+    } else if (isDraw()) {
+        endGame(true);
+    } else {
+        switchTurn();
+        setBorderHoverClass();
+    }
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
+}
+
+function switchTurn() {
+    circleTurn = !circleTurn;
+}
+
+function setBorderHoverClass() {
+    board.classList.remove(CIRCLE_CLASS);
+    board.classList.remove(X_CLASS);
+
+    if (circleTurn) board.classList.add(CIRCLE_CLASS);
+    else board.classList.add(X_CLASS);
+}
+
+function checkWin(currentClass) {
+    return WINNING_COMBINATIONS.some((combination) => {
+        return combination.every((index) => {
+            return cellElements[index].classList.contains(currentClass);
+        });
+    });
+}
+
+function endGame(draw) {
+    if (draw) {
+        winnerMessageText.textContent = "Draw!";
+    } else {
+        winnerMessageText.textContent = `${circleTurn ? "O's" : "X's"} Wins!`;
+        if (circleTurn) {
+            oScore++;
+        } else {
+            xScore++;
+        }
+    }
+
+    winnerMessageElement.classList.add("show");
+}
+
+function isDraw() {
+    return [...cellElements].every((cell) => {
+        return (
+            cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+        );
+    });
+}
